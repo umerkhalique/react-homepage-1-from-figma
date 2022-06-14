e7b61d87f541f249b923b0125ee53c07347addf5
umer khalique <omerkhalique13@gmail.com>
2022-06-14 16:22:39 +0500

    final

---
+++ src/components/Header.js
@@ -0,0 +1,48 @@
+import React, { Fragment } from "react";
+import logo from "../assets/logo.png";
+import "../App.css";
+function Header() {
+  return (
+    <Fragment>
+      <nav className="nav">
+        <div className="logo">
+          <img src={logo} />
+        </div>
+        <div className="nav-group">
+          <ul>
+            <li className="li-1">
+              <a href="#">Features</a>
+              <i class="fa-solid fa-angle-down"></i>
+            </li>
+            <li className="li-2">
+              <a href="#">Company</a>
+              <i class="fa-solid fa-angle-down"></i>
+            </li>
+            <li className="li-3">
+              <a href="#">Careers</a>
+            </li>
+            <li className="li-4">
+              <a href="#">About</a>
+            </li>
+            <div className="login-group">
+              <div className="login-1">
+                <li className="li-5">
+                  <a href="#">Login</a>
+                </li>
+              </div>
+              <div className="login-2">
+                <div className="login-3">
+                  <li className="li-6">
+                    <a href="#">Register</a>
+                  </li>
+                </div>
+              </div>
+            </div>
+          </ul>
+        </div>
+      </nav>
+    </Fragment>
+  );
+}
+
+export default Header;
