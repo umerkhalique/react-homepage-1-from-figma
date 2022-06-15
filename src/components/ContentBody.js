1571d978c601eb36eae5d48d76c2876ec4ed8827
umer khalique <omerkhalique13@gmail.com>
2022-06-16 01:09:58 +0500

    commit

--- src/components/ContentBody.js
+++ src/components/ContentBody.js
@@ -1,24 +1,40 @@
-import React, { Fragment } from 'react'
-import image from "../assets/image.png"
+import React, { Fragment } from "react";
+import image from "../assets/image.png";
 import "../App.css";
+import image1 from '../assets/client-databiz.svg'
+import image2 from '../assets/client-audiophile.svg'
+import image3 from '../assets/client-meet.svg'
+import image4 from '../assets/client-maker.svg'
 function ContentBody() {
   return (
-    <Fragment className="frag">
-      <div className="container">
+    <Fragment>
+      <div>
+        <div>
+          <h1 className="h1-heading">Make remote work</h1>
           <div>
-            <div className="div-heading">
-              <h1 className="h1-heading">Make remote work</h1>
-              </div>
-            <div></div>
-            <div></div>
+            <p className="para">
+              Get your team in sync, no matter your location. Streamline
+              processes, create team rituals, and watch productivity soar.
+            </p>
           </div>
-          <div></div>
+          <div className="btn">
+            <button className="btn-inner">
+              <p className="ppp">Learn more</p>
+            </button>
+          </div>
+        </div>
+        
+          <img className="img11" src={image1} />
+          <img className="img22" src={image2} />
+          <img className="img33" src={image3} />
+          <img className="img44" src={image4} />
+        
       </div>
       <div>
-          <img className="img-1" src={image}/>
+        <img className="img-1" src={image} />
       </div>
     </Fragment>
-  )
+  );
 }
 
-export default ContentBody
+export default ContentBody;
